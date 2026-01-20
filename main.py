7cff48d55dbd0c437d55ee6adf0547fbee911dcd
adhikariiii <prabitadhikari10@gmail.com>
2026-01-20 08:09:46 +0545

    third commit

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
-from PyQt5.QtWidgets import QLabel, QWidget, QApplication, QVBoxLayout
-from PyQt5.QtGui import QFont, QFontDatabase
-from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtCore import QTime, QTimer, Qt
 import sys
-import os
 
-class Digitalclock(QWidget):
+class DigitalClock(QWidget):
     def __init__(self):
         super().__init__()
         self.time_label = QLabel(self)
         self.timer = QTimer()
-    
-        self.initUI()
 
+ 
+        self.initUI()
     
     def initUI(self):
-        self.setGeometry(1100, 0 , 600, 300)
-
+        self.setGeometry(1100, 0, 300 , 400)
 
         vbox = QVBoxLayout()
-
         vbox.addWidget(self.time_label)
+
         self.setLayout(vbox)
         vbox.setAlignment(Qt.AlignCenter)
 
         self.setStyleSheet("font-size:100px; background-color:black; color:#39FF14;")
-        # font_id = QFontDatabase.addApplicationFont("DS-DIGIB.TTF")
-
-
-
-        # font_id = QFontDatabase.addApplicationFont(
-        # os.path.join(os.path.dirname(__file__), "DS-DIGIB.TTF")
-        #            )
-        # self.time_label.setFont(QFont(
-        #  QFontDatabase.applicationFontFamilies(font_id)[0], 150
-        #           ))
-
-        # font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
-        # print(font_id)
-        # font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
-
-        # my_font = QFont(font_family, 150)
-        # self.time_label.setFont(my_font)
-
         self.timer.timeout.connect(self.update_time)
         self.timer.start(1000)
-
         self.update_time()
 
     def update_time(self):
@@ -52,10 +30,9 @@ class Digitalclock(QWidget):
         self.time_label.setText(current_time)
 
 
-
 def main():
     app = QApplication(sys.argv)
-    clock = Digitalclock()
+    clock = DigitalClock()
     clock.show()
     sys.exit(app.exec_())
 
